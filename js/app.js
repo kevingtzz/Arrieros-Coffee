2908fce95e4386e5fc6bb9f1d1852c777c02e4e7
Kevin Gutierrez <kevingtzz@Kevins-MacBook-Air-2.local>
2019-03-30 00:23:12 -0500

    navbar comportament and desing finished

--- js/app.js
+++ js/app.js
@@ -1,49 +1,11 @@
-var nav = document.getElementById("navBar")
-
-// Nav var animations
-//var lastPagePosition = pageYOffset
-
-
-/*window.addEventListener("scroll", () => {
-    if (pageYOffset < lastPagePosition && screen.width > 500) {
-        nav.style.background = "rgba(26,23,30,.7)"
-        nav.style.boxShadow = "0px 3px 6px rgba(26,23,30,.7)"
-        nav.style.top = "0"
-        setTimeout(() => {
-            let tourPosition = document.getElementById("tours").getBoundingClientRect().top
-            let productsPosition = document.getElementById("products").getBoundingClientRect().top
-            let aboutPosition = document.getElementById("about").getBoundingClientRect().top
-            let homePosition = document.getElementById("home").getBoundingClientRect().top
-            if (tourPosition == 0 || productsPosition == 0 || aboutPosition == 0) {
-                nav.style.top = "-60px"
-            } else if (homePosition == 0) {
-                nav.style.background = "none"
-                nav.style.boxShadow = "none"
-            }
-        }, 10)
-    } else if (pageYOffset > lastPagePosition && screen.width > 500) {
-        nav.style.top = "-60px"
-    }
-    lastPagePosition = pageYOffset
-})*/
-window.addEventListener("scroll", ()=> {
-    if (pageYOffset > 100) {
-        nav.style.background = "rgb(242, 239, 234)"
-        nav.style.boxShadow = "0px 3px 4px rgba(0,0,0,.5)"
-        document.querySelectorAll('nav ul li a')[0].style.color = "rgb(80, 77, 72)"
-        document.querySelectorAll('nav ul li a')[1].style.color = "rgb(80, 77, 72)"
-        document.querySelectorAll('nav ul li a')[2].style.color = "rgb(80, 77, 72)"
-        document.querySelectorAll('nav ul li a')[3].style.color = "rgb(80, 77, 72)"
-        document.querySelectorAll('nav ul li a')[4].style.color = "rgb(80, 77, 72)"
-    } else {
-        nav.style.background = "none"
-        nav.style.boxShadow = "none"
-        document.querySelectorAll('nav ul li a')[0].style.color = "aliceblue"
-        document.querySelectorAll('nav ul li a')[1].style.color = "aliceblue"
-        document.querySelectorAll('nav ul li a')[2].style.color = "aliceblue"
-        document.querySelectorAll('nav ul li a')[3].style.color = "aliceblue"
-        document.querySelectorAll('nav ul li a')[4].style.color = "aliceblue"
-    }
+//Navbar animation
+var menu = document.getElementById('menu-icon')
+menu.addEventListener("click", ()=>{
+    let menuList = document.getElementById('menu-list')
+    menuList.style.transform = 'none'
+    menuList.addEventListener("click", ()=>{
+        menuList.style.transform = 'translateY(-180px)'
+    })
 })
 
 
