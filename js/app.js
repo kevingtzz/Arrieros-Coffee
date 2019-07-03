f4623a574bb0f1c0967264a13d3ae412db536045
Kevin Gutierrez Gomez <kevingtzz@Kevins-MacBook-Air.local>
2019-07-03 02:28:37 -0500

    scroll menu animations done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 //Navbar animation
+
 var menu = document.getElementById('menu-icon')
+var menu_desktop = document.getElementById('menu-list')
+var home = document.getElementById("home")
+var links = document.querySelectorAll("nav a")
+console.log(links)
+
 menu.addEventListener("click", ()=>{
     let menuList = document.getElementById('menu-list')
     menuList.classList.add('showMenu')
@@ -10,7 +16,7 @@ menu.addEventListener("click", ()=>{
 
 var lastPagePosition = pageYOffset
 window.addEventListener("scroll", () => {
-    if (pageYOffset > lastPagePosition) {
+    if (pageYOffset > lastPagePosition && screen.width < 800) {
         menu.style.transform = "translateY(-65px)"
         setTimeout(() => {
             let tourPosition = document.getElementById("tours").getBoundingClientRect().top
@@ -21,10 +27,43 @@ window.addEventListener("scroll", () => {
                 menu.style.transform = "translateY(-65px)"
             } 
         }, 10)
-    } else if (pageYOffset < lastPagePosition) {
+    } else if (pageYOffset < lastPagePosition && screen.width < 800) {
         menu.style.transform = "translateY(0px)"
+    } else {
+        if (pageYOffset > lastPagePosition) { // Desktop nav animation
+            menu_desktop.style.transform = "translateY(-65px)"
+            setTimeout(() => {
+                let tourPosition = document.getElementById("tours").getBoundingClientRect().top
+                //let productsPosition = document.getElementById("products").getBoundingClientRect().top
+                //let aboutPosition = document.getElementById("about").getBoundingClientRect().top
+                let homePosition = home.getBoundingClientRect().top
+                if (tourPosition == 0 /*|| productsPosition == 0 || aboutPosition == 0*/) {
+                    menu_desktop.style.transform = "translateY(-65px)"
+                } 
+            }, 10)
+        } else if (pageYOffset < lastPagePosition) {
+            menu_desktop.style.transform = "translateY(0px)"
+        }
     }
     lastPagePosition = pageYOffset
+
+    if (home.getBoundingClientRect().top < -150) {          //turns background home transparent when client is on home.
+        menu_desktop.style.background = "white";
+        for (i = 1; i < links.length; i++) {
+            links[i].style.color = "rgb(63, 63, 63)"
+            links[i].style.textShadow = "none"
+        }
+        document.getElementById('home-icon').style.color = "rgb(63, 63, 63)"
+    } else {
+        if (screen.width >= 800) {
+            menu_desktop.style.background = "transparent";
+            for (i = 1; i < links.length; i++) {
+                links[i].style.color = "rgb(214, 214, 214)"
+                links[i].style.textShadow = "0px 0.5px 2px black"
+            }
+            document.getElementById('home-icon').style.color = "rgb(214, 214, 214)"
+        }
+    }
 })
 
 
